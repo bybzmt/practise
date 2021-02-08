8c6cc409f6d25a71c15b9b61a097c5a21d6a6d80
bybzmt <other@bybzmt.me>
2021-02-08 13:43:23 +0800

    ok

--- mcu/digital-amplifier2/firmware/Src/bsp_audio.c
+++ mcu/digital-amplifier2/firmware/Src/bsp_audio.c
@@ -5,17 +5,19 @@
 #include "stm32f4xx_ll_dma.h"
 
 static uint32_t audio_freq;
-static uint32_t audio_buffer[4096];
-static int16_t audio_buffer_size;
-static uint16_t audio_sample_size;
-static uint16_t audio_sample_max;
-static volatile int16_t audio_wptr;
-static volatile int16_t audio_sample_count;
-static volatile uint8_t audio_state;
+static uint8_t audio_buffer[8192];
+static uint16_t audio_buffer_size;
 static uint16_t audio_buffer_max;
+static uint16_t audio_sample_size;
+static uint32_t audio_sample_max;
+static volatile uint32_t audio_sample_write;
+static volatile uint32_t audio_sample_read;
+static volatile uint8_t audio_state;
 
 #define AUDIO_STATE_HALF 1
 #define AUDIO_STATE_SYNC 2
+#define AUDIO_STATE_FAST 4
+#define AUDIO_STATE_SLOW 8
 
 static void BSP_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai);
 static void BSP_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai);
@@ -45,15 +47,15 @@ void BSP_AUDIO_Init(uint32_t AudioFreq, uint8_t AudioSize, uint16_t bufferSize, 
     __HAL_SAI_ENABLE(&hsai_out);
 
     memset(audio_buffer, 0, sizeof(audio_buffer));
-    audio_wptr = 0;
-    audio_sample_count = 0;
+    audio_sample_write = 0;
+    audio_sample_read = 0;
     audio_state = 0;
     audio_buffer_size = bufferSize;
     audio_sample_size = 4;
     audio_buffer_max = ((sizeof(audio_buffer) / audio_buffer_size) - 1) * audio_buffer_size;
     audio_sample_max = audio_buffer_max / audio_sample_size;
 
-    HAL_StatusTypeDef ret = HAL_SAI_Transmit_DMA(&hsai_out, (uint8_t*)audio_buffer, audio_buffer_max/2);
+    HAL_StatusTypeDef ret = HAL_SAI_Transmit_DMA(&hsai_out, &audio_buffer[0], audio_buffer_max/2);
     if (ret!= HAL_OK) {
         printf("play err:%d\n", ret);
     }
@@ -64,32 +66,31 @@ void BSP_AUDIO_Init(uint32_t AudioFreq, uint8_t AudioSize, uint16_t bufferSize, 
 
 uint8_t *BSP_AUDIO_AllocBuffer(void)
 {
-    return (uint8_t*)(&audio_buffer[audio_wptr/4]);
+    /* if (audio_state & AUDIO_STATE_FAST) { */
+        /* audio_sample_write--; */
+    /* } else if (audio_state & AUDIO_STATE_SLOW) { */
+        /* audio_sample_write++; */
+    /* } */
+
+    return &audio_buffer[(audio_sample_write % audio_sample_max)*4];
 }
 
 void BSP_AUDIO_PostBuffer(uint16_t len)
 {
-    if ( !(audio_state & AUDIO_STATE_SYNC) ) {
-        if (audio_state & AUDIO_STATE_HALF) {
-            audio_wptr = audio_buffer_max / 2;
-        }
+    audio_state |= AUDIO_STATE_SYNC;
 
-        audio_state |= AUDIO_STATE_SYNC;
-    }
+    uint32_t sample_add = len / audio_sample_size;
 
-    audio_sample_count += len / audio_sample_size;
+    uint32_t sample_over = (audio_sample_write % audio_sample_max) + sample_add;
 
-    if (audio_wptr + len < audio_buffer_max) {
-        audio_wptr = audio_wptr + len;
-    } else {
-        audio_wptr = audio_wptr + len - audio_buffer_max;
+    audio_sample_write += sample_add;
 
-        uint32_t *dst, *src;
+    if (sample_over > audio_sample_max) {
+        uint8_t *dst, *src;
         dst = &audio_buffer[0];
-        src = &audio_buffer[audio_buffer_max/4];
-
-        int32_t end = audio_wptr/4;
-        while (end--) {
+        src = &audio_buffer[audio_sample_max*4];
+        sample_over = (sample_over % audio_sample_max)*4;
+        while (sample_over--) {
             *dst++ = *src++;
         }
     }
@@ -122,43 +123,49 @@ void BSP_AUDIO_DeInit(void)
 
 static void bsp_sai_sync(void)
 {
-    audio_sample_count -= audio_sample_max / 2;
+    if (audio_state & AUDIO_STATE_SYNC) {
+        audio_sample_read += audio_sample_max / 2;
 
-    if (audio_sample_count > audio_sample_max/4) {
-        /* audio_sample_count -= 1; */
-        audio_wptr -= audio_buffer_size;
+        uint32_t base = audio_sample_max / 4;
 
-        if (audio_wptr < 0) {
-            audio_wptr += audio_buffer_max;
+        if (audio_sample_read > 0xfff00000U) {
+            audio_state &= ~AUDIO_STATE_SYNC;
+        } else if (audio_sample_read > audio_sample_write-base) {
+            audio_sample_write += audio_sample_read + (audio_sample_max/2) - audio_sample_write;
+        } else if (audio_sample_read+(base*3) < audio_sample_write) {
+            audio_sample_write -= audio_sample_write - (base*2) - audio_sample_read;
         }
-        audio_sample_count = 0;
-    } else if (audio_sample_count < -(audio_sample_max/4) ) {
-        /* audio_sample_count += 1; */
-        audio_wptr += audio_buffer_size;
 
-        if (audio_wptr >= audio_buffer_max) {
-            audio_wptr -= audio_buffer_max;
+        /* uint32_t base = audio_sample_max / 2; */
+        /* if (audio_sample_read > 0xfff00000U) { */
+            /* audio_state &= ~AUDIO_STATE_SYNC; */
+        /* } else if (audio_sample_read > audio_sample_write-base) { */
+            /* audio_state |= AUDIO_STATE_SLOW; */
+        /* } else if (audio_sample_read+base < audio_sample_write) { */
+            /* audio_state |= AUDIO_STATE_FAST; */
+        /* } else { */
+            /* audio_state &= ~(AUDIO_STATE_FAST|AUDIO_STATE_SLOW); */
+        /* } */
+    } else {
+        audio_sample_read = 0;
+        if (audio_state & AUDIO_STATE_HALF) {
+            audio_sample_write = audio_sample_max / 2;
+        } else {
+            audio_sample_write = 0;
         }
-        audio_sample_count = 0;
     }
 }
 
 static void BSP_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai)
 {
     audio_state |= AUDIO_STATE_HALF;
-
-    if (audio_state & AUDIO_STATE_SYNC) {
-        bsp_sai_sync();
-    }
+    bsp_sai_sync();
 }
 
 static void BSP_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai)
 {
     audio_state &= ~AUDIO_STATE_HALF;
-
-    if (audio_state & AUDIO_STATE_SYNC) {
-        bsp_sai_sync();
-    }
+    bsp_sai_sync();
 }
 
 static void BSP_SAI_ErrorCallback(SAI_HandleTypeDef *hsai)
