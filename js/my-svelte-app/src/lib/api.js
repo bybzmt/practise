82823c860c59bbdd2e00c5640cf8a4b72c454eb1
bybzmt <other@bybzmt.me>
2022-01-17 18:19:33 +0800

    save

---
+++ js/my-svelte-app/src/lib/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const api_url = API_BASE + '/mcn/graphql';
+
+function GraphQLInit(context) {
+	return async function (query, variables) {
+		let cookie = context.get("cookie")
+		let sid = cookie.sid || ""
+
+		const data = {
+			sid: sid,
+			query: query,
+			variables: variables,
+		}
+
+		const resp = await axios.post(api_url, data)
+
+		if (resp.data.errors) {
+			throw new Error(resp.data.errors[0].debugMessage || resp.data.errors[0].message);
+		}
+
+		if (resp.data.data) {
+			return resp.data
+		}
+
+		throw new Error(resp.data);
+	}
+}
+
+export { GraphQLInit }
